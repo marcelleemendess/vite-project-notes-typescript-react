5142cbb0e441b64e0dfc000ecc99bdd937e45c39
Marcelle Mendes <marcellee.mendess@gmail.com>
2023-01-10 20:21:38 -0300

    add new components and bootstrap style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
+import { NewNote } from "./NewNote"
 
 function App() { 
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/new" element={<h1>New</h1>} />
-      <Route path="/:id">
-        <Route index element={<h1>Show</h1>} />
-        <Route path="edit" element={<h1>Edit</h1>} />
-      </Route>
-      {/* Navigate comp. to when the user navigate to a url unavailable, navigates back */}
-      <Route path="*" element={<Navigate to="/"/>} />
-    </Routes>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        {/* Navigate comp. to when the user navigate to a url unavailable, navigates back */}
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
+    </Container>
   )  
 }
 
