9be4260687db417aeb0f46fa59c2f626edabada7
Marcelle Mendes <marcellee.mendess@gmail.com>
2023-01-23 09:02:36 -0300

    add localStorage logic, add logic to Create Notes and Tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useMemo } from "react"
 import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 import { NewNote } from "./NewNote"
+import { useLocalStorage } from "./useLocalStorage"
+import { v4 as uuidV4 } from "uuid"
 
 //adds id to the the existing NoteData type
 export type Note = {
@@ -10,7 +13,7 @@ export type Note = {
 
 export type RawNote = {
   id: string
-}
+} & RawNoteData
 
 export type RawNoteData = {
   title: string
@@ -31,7 +34,29 @@ export type Tag = {
 
 function App() { 
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  //localStorage to persist the information
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  //convert raw notes into real notes
+  const notesWithTags = useMemo(() => {
+    //loop through all my different notes
+    return notes.map(note => {
+      //for each note in keep all the information about the notes but also get the tags that have all the associated ID inside of the note that's being stored
+      return {...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))
+      }
+    })
+  }, [notes, tags]) //just run when the notes or tags get updated
+
+  //function to create notes
+  function onCreateNotes({tags, ...data}: NoteData) {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes, 
+        {...data, id: uuidV4(), tagsIds: tags.map(tag => tag.id) },
+      ]
+    })
+  }
+
   return (
     <Container className="my-4">
       <Routes>
--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,6 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+// T -> generic type
 export function useLocalStorage<T>(key: string,
     initialValue: T | (() => T)) {
-        
+        const [ value, setValue] = useState<T>(() => {
+            const jsonValue = localStorage.getItem(key)
+            //check if the value exists
+            if (jsonValue === null) {
+                if(typeof initialValue === "function") {
+                    return (initialValue as () => T)()
+                    } else {
+                        return initialValue
+                    }
+            } else {
+                return JSON.parse(jsonValue)
+            }
+        })
+
+        // everytime the value changes, update it.
+        useEffect(() => {
+            localStorage.setItem(key, JSON.stringify(value))
+        },[value, key])
+        return [ value, setValue ] as [T, typeof setValue]
     }
